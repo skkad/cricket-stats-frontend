37da6a0f56f6a48ae08f9f264fe53dbe2d5abd65
skkad <skkad1998@gmail.com>
2025-06-26 00:13:34 +0530

    feat: app with pwa

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
+const withPWA = require("next-pwa")({
+  dest: "public", // folder for generated service worker
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: "export",
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,9 @@ export default function Document() {
         {process.env.NODE_ENV === "development" && (
           <meta name="next-head-count" content="3" />
         )}
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon192.png" />
+        <meta name="theme-color" content="#317EFB" />
       </Head>
       <body>
         <Main />
